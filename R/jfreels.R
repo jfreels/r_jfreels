0c27c3b399fb1a92f6a5901b1539fc383b4c66f8
Justin Freels <jfreels@gmail.com>
2013-04-15 15:38:29 -0500

    fixed names for xtsToLong

--- R/jfreels.R
+++ R/jfreels.R
@@ -41,6 +41,6 @@ xtsToLong<-function (Xts) {
   df<-data.frame(date=index(Xts),coredata(Xts)) 
   names(df)<-c("date",names(Xts))
   df<-melt(df,id.vars="date")
-  names(df)<-c("date","fund","return")
+  names(df)<-c("date","variable","value")
   df
 }
