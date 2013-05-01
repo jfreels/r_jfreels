1c2d57f4d49fe9cb0297b5efb9c8286d84bb0928
Justin Freels <jfreels@gmail.com>
2013-05-01 11:59:26 -0500

    test

--- R/charts.R
+++ R/charts.R
@@ -15,7 +15,8 @@
   }
   
   jf.dates<-function(DF) {
-    dat<-ddply(DF,.(variable),summarise,start_date=min(date),end_date=max(date))
+    dat<-data.frame(date=DF$date,variable=DF$variable,value=DF$value)
+    dat<-ddply(dat,.(variable),summarise,start_date=min(date),end_date=max(date))
     list(all=dat,
          max_start_date=max(dat$start_date),
          min_end_date=min(dat$end_date)
