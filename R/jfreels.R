2e115de88d1f5ba20d227f2b53518b76042e1d5f
Justin Freels <jfreels@gmail.com>
2013-05-01 16:01:02 -0500

    added jf.cor.quant function

--- R/jfreels.R
+++ R/jfreels.R
@@ -36,6 +36,19 @@ jf.stats<-function (longDataFrame) {
         end=max(date))
 }
 
+jf.cor.quant<-function(DF,primary) {
+  dat<-data.frame(date=DF$date,variable=DF$variable,value=DF$value)
+  dat<-na.omit(dcast(dat,date~variable,value.var="value"))
+  dat<-melt(dat,id.vars="date")
+  cor.table<-jf.cor(dat)
+  cor.table<-subset(cor.table,Var2==primary)
+  cor.table<-arrange(cor.table[-2],-value)
+  names(cor.table)<-c("variable","cor")
+  quant.table<-jf.stats(dat)
+  cor.quant.table<-merge(cor.table,quant.table,by="variable")
+  arrange(cor.quant.table,-cor)
+}
+
 dt.stats<-function (dataTable) {
   # data should have columns "date","variable","value"
   dataTable[,list(cror=cror(value),
