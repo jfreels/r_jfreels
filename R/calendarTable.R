62451e6907d2753b9d606c854c917a4b96c49a99
Justin Freels <jfreels@gmail.com>
2013-08-05 17:17:33 -0500

    tweaked calendarTable.R

--- R/calendarTable.R
+++ R/calendarTable.R
@@ -1,10 +1,10 @@
-calendarTable<-function (oneFundLongDataFrame) {
+calendarTable<-function (oneFundLongDataFrame,ITD=FALSE) {
   calendarTable<-dcast(oneFundLongDataFrame,year(date)~month(date),value.var="value")
   names(calendarTable)<-c("Year",month.abb)
   calendarTable<-arrange(calendarTable,-Year)
   row.names(calendarTable)<-calendarTable[,1]
   calendarTable<-calendarTable[,-1]
   calendarTable$YTD<-apply(calendarTable,1,aror)
-  calendarTable$ITD<-rev(vami(rev(calendarTable$YTD))-1)
+  if(ITD==TRUE) { calendarTable$ITD<-rev(vami(rev(calendarTable$YTD))-1) }
   calendarTable
 }
