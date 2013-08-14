ca23fc579df5ecbfdcc53322c9c892816c4a8508
Justin Freels <jfreels@gmail.com>
2013-08-14 09:45:37 -0500

    Revert "added dt.xts.R"
    This reverts commit ce11e96e5b3e79fd346d258144e98fe8f24c5e43.

--- R/dt.xts.R
+++
@@ -1,7 +0,0 @@
-# convert data.table to xts object
-dt.xts <- function (dt) {
-  require(xts)
-  dt_dcast <- dcast(dt,date~variable,value.var='value')
-  dt_xts <- xts(dt_dcast[,-1],dt_dcast[,1])
-  dt_xts
-}
