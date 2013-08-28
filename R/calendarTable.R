76dccc27908045e628b7aadab7d6daa43403fe88
Justin Freels <jfreels@gmail.com>
2013-08-28 15:28:07 -0500

    fixed calendarTable.R

--- R/calendarTable.R
+++ R/calendarTable.R
@@ -1,9 +1,15 @@
-calendarTable<-function (oneFundLongDataFrame,ITD=FALSE) {
-  calendarTable<-dcast(oneFundLongDataFrame,year(date)~month(date),value.var="value")
+calendarTable<-function (DT,ITD=FALSE) {
+  DT<-DT[,list(date,value)]
+  if(nrow(DT<12)) {
+    calendarBlank<-data.frame(date=(max(DT$date)+days(1))+months(1:12)-days(1),value=rep(NA,12))
+    calendarTable<-rbind(DT,calendarBlank)
+  }
+  calendarTable<-dcast(calendarTable,year(date)~month(date),value.var="value")
   names(calendarTable)<-c("Year",month.abb)
   calendarTable<-arrange(calendarTable,-Year)
   row.names(calendarTable)<-calendarTable[,1]
   calendarTable<-calendarTable[,-1]
+  calendarTable<-calendarTable[apply(calendarTable,1,function(x) sum(is.na(x)))!=12,]
   calendarTable$YTD<-apply(calendarTable,1,aror)
   if(ITD==TRUE) { calendarTable$ITD<-rev(vami(rev(calendarTable$YTD))-1) }
   calendarTable
