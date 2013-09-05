001b3be11c92fe1b1cefb48e21b6e9a0de055232
Justin Freels <jfreels@gmail.com>
2013-09-05 12:01:36 -0500

    fixed dt.xts.R

--- R/dt.xts.R
+++ R/dt.xts.R
@@ -1,7 +1,9 @@
 # convert data.table to xts object
 dt.xts <- function (dt) {
   require(xts)
+  dt_names<-unique(dt$variable)
   dt_dcast <- dcast(dt,date~variable,value.var='value')
   dt_xts <- xts(dt_dcast[,-1],dt_dcast[,1])
+  names(dt_xts)<-dt_names
   dt_xts
 }
