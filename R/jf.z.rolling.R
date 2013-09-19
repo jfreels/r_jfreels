6f2876418b9e2975e84919998f28eafed1ed5022
Justin Freels <jfreels@gmail.com>
2013-09-19 14:21:58 -0500

    fixed jf.z.rolling.R

--- R/jf.z.rolling.R
+++ R/jf.z.rolling.R
@@ -7,8 +7,8 @@ jf.z.rolling <- function (name, n=12,DT=z) {
   dat[,roll.asd:=rollapply(value,width=n,fill=NA,align='right',FUN=asd),by=variable]
   dat_title <- paste0(unique(dat$variable),"\nRolling ",n," Month Return and Standard Deviation (Annualized)\n",head(dat$date,1)," to ",tail(dat$date,1))
   p<-ggplot(dat,aes(x=date))+
-    geom_line(aes(y=roll.ror),color=col.brew[8])+
-    geom_line(aes(y=roll.asd),color=col.brew[4])+
+    geom_line(aes(y=roll.ror),color=col.brew[8],size=2)+
+    geom_line(aes(y=roll.asd),color=col.brew[4],size=2)+
     geom_hline(yintercept=0,color='black',linetype=3)+
     labs(title=dat_title,x=NULL,y=paste0(n,' Month Rolling Return (blue) and Standard Deviation (orange)'))+
     scale_y_continuous(labels=percent)+
