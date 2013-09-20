93cc56df664e208ea027bd45e8618186dc87757a
Justin Freels <justin.freels@meritagecapital.com>
2013-09-20 14:58:23 -0500

    fixed dt.dates.filter

--- R/dt.dates.filter.R
+++ R/dt.dates.filter.R
@@ -1,3 +1,3 @@
 dt.dates.filter<-function(DT) {
-  DT[date>=max(dt.dates(DT)$data_start)][date<=max(dt.dates(DT)$data_end)]
+  DT[date>=max(dt.dates(DT)$data_start)][date<=min(dt.dates(DT)$data_end)]
 }
