eb5789532a1ca906d2fcca7d2c6764e666e3cbf4
Justin Freels <justin.freels@meritagecapital.com>
2013-09-30 17:54:00 -0500

    added calmar and pain functions to jfreels.R

--- R/jfreels.R
+++ R/jfreels.R
@@ -15,6 +15,8 @@ asd<-function (ror,periods=12) { sd(ror) * sqrt(periods) }
 sharpe<-function (ror,periods=12) { aror(ror,periods)/asd(ror,periods) }
 maxdd<-function (ror) { min(dd(ror)) }
 dd<-function (ror) { -(1 - vami(ror)/cummax(c(1, cummax(vami(ror))))[-1]) }
+calmar<-function (ror,...) { aror(ror,...)/abs(maxdd(ror)) }
+pain<-function (ror,...) { aror(ror,...)/abs(mean(dd(ror))) }
 hwm<-function (ror) { 1/(1+tail(dd(ror),1))-1 }
 omega<-function (ror) { sum(ror[ror>0])/sum(abs(ror[ror<0])) }
 percentUp<-function (ror) { values<-na.omit(ror); length(values[values>0])/length(values) }
