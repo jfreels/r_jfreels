8cc2d4b5cc3e8275fb822171b0912607cfce894e
Justin Freels <justin.freels@meritagecapital.com>
2014-07-18 13:01:07 -0500

    fixed jf.tr.table.R

--- R/jf.tr.table.R
+++ R/jf.tr.table.R
@@ -1,31 +1,60 @@
-jf.tr.table<-function(DT,asof,allocation=FALSE) {
-  setkey(DT,variable,date)
-	DT.variables<-data.table(variable=unique(DT$variable),key='variable')
-	asof<-as.Date(asof)
-	ifelse(allocation==TRUE,
-	       MTD<-DT[date==asof,list(date,variable,Allocation=allocation,MTD=value)],
-	       MTD<-DT[date==asof,list(date,variable,MTD=value)])
-	QTD<-DT[quarter(date)==quarter(asof)][year(date)==year(asof)][,list(QTD=cror(value)),by=variable]
-	dat<-join(MTD,QTD,by='variable')
-	YTD<-DT[year(date)==year(asof),list(YTD=cror(value)),by=variable]
-	dat<-join(dat,YTD,by='variable')
-	# managers with track records > 12 months
-	names12<-as.character(DT[,.N-11,by=variable][V1>0]$variable)
-	last12<-DT[names12,.SD[(.N-11):(.N)]][,list(Last12=cror(value)),by=variable]
-	dat<-join(dat,last12,by='variable')
-	names24<-as.character(DT[,.N-23,by=variable][V1>0]$variable)
-	#last24<-DT[names24,.SD[(.N-23):(.N)]][,list(Last24=cror(value)),by=variable]
-	#dat<-join(dat,last24,by='variable')
-	names36<-as.character(DT[,.N-35,by=variable][V1>0]$variable)
-	last36<-DT[names36,.SD[(.N-35):(.N)]][,list(Last36=cror(value)),by=variable]
-	dat<-join(dat,last36,by='variable')
-	names60<-as.character(DT[,.N-59,by=variable][V1>0]$variable)
-	last60<-DT[names60,.SD[(.N-59):(.N)]][,list(Last60=cror(value)),by=variable]
-	dat<-join(dat,last60,by='variable')
-	setkey(dat,variable)
-	dat<-dat[DT.variables] # merge dat and DT.variables in order to keep all the names in the original dataset
-	ifelse(allocation==TRUE,
-	       setcolorder(dat,neworder=c('date','variable','Allocation','MTD','QTD','YTD','Last12','Last36','Last60')),
-	       setcolorder(dat,neworder=c('date','variable','MTD','QTD','YTD','Last12','Last36','Last60')))
-	dat
+jf.tr.table<-function(df,asof,allocation=FALSE) {
+  # set up variables
+  asof<-as.Date(asof)
+  df<-df %>%
+    group_by(variable) %>%
+    arrange(variable,date) %>%
+    filter(date<=asof)
+  # Month to date value
+  MTD<-df %>%
+    filter(date==as.Date(asof))
+  ifelse(allocation==TRUE,
+         {MTD<-MTD %>%
+           select(date,variable,Allocation=allocation,MTD=value)},
+         {MTD<-MTD %>%
+           select(date,variable,MTD=value)})
+  # Quarter to date value
+  QTD<-df %>%
+    filter(quarter(date)==quarter(asof),year(date)==year(asof)) %>%
+    summarise(QTD=cror(value))
+  dat<-left_join(MTD,QTD,by='variable')
+  # Year to date value
+  YTD<-df %>%
+    filter(year(date)==year(asof)) %>%
+    summarise(YTD=cror(value))
+  dat<-left_join(dat,YTD,by='variable')
+  # managers with track records > 12 months
+  names12<-df %>%
+    tally() %>%
+    filter(n>=12) %>%
+    .$variable
+  last12<-df %>%
+    filter(variable %in% names12,
+           min_rank(desc(date))<=12) %>%
+    summarise(Last12=cror(value))
+  dat<-left_join(dat,last12,by='variable')
+  # managers with track records > 36 months
+  names36<-df %>%
+    tally() %>%
+    filter(n>=36) %>%
+    .$variable
+  last36<-df %>%
+    filter(variable %in% names36,
+           min_rank(desc(date))<=36) %>%
+    summarise(Last36=cror(value))
+  dat<-left_join(dat,last36,by='variable')
+  # managers with track records > 60 months
+  names60<-df %>%
+    tally() %>%
+    filter(n>=60) %>%
+    .$variable
+  last60<-df %>%
+    filter(variable %in% names60,
+           min_rank(desc(date))<=60) %>%
+    summarise(Last60=cror(value))
+  dat<-left_join(dat,last60,by='variable')
+  ifelse(allocation==TRUE,
+         dat<-arrange(dat,desc(Allocation)),
+         dat<-arrange(dat,desc(MTD)))
+  dat
 }
