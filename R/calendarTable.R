59d3c5d0896d7aa8eb27b2ad2ed1fad312c2bab3
Justin Freels <justin.freels@meritagecapital.com>
2014-07-21 16:10:28 -0500

    fixed calendarTable.R

--- R/calendarTable.R
+++ R/calendarTable.R
@@ -3,16 +3,10 @@ calendarTable<-function (df,ITD=FALSE) {
   df<-df %>%
     group_by() %>%
     select(date,value)
-  df_years<-unique(year(df$date))
-  df_dates<-df_years %>%
-    lapply(function(x) {
-      as.Date(paste(x,seq(1,12),'01',sep='-'),format='%Y-%m-%d')+months(1)-days(1)
-    }) %>%
-    unlist %>%
-    as.Date
+  df_dates<-jf.dates.completed(df)
   df_blank<-data.frame(date=df_dates)
-  df<-left_join(df_blank,df,by='date')
-  calendarTable<-dcast(df,year(date)~month(date),value.var="value")
+  df_joined<-left_join(df_blank,df,by='date')
+  calendarTable<-dcast(df_joined,year(date)~month(date),value.var="value")
   names(calendarTable)<-c("Year",month.abb)
   calendarTable<-arrange(calendarTable,desc(Year))
   row.names(calendarTable)<-calendarTable[,1]
