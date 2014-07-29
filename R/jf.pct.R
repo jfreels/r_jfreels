bef86c6f3851c7232ea55494664ec0ba72818a18
Justin Freels <justin.freels@meritagecapital.com>
2014-07-29 16:41:42 -0500

    fixed jf.pct.R

--- R/jf.pct.R
+++ R/jf.pct.R
@@ -1,1 +1,8 @@
-jf.pct <- function(x) { paste0(round(x*100,2),'%') }
+jf.pct <- function(numeric) { 
+	if(!is.numeric(numeric)) stop('Function requires numeric value.')
+	numeric %>%
+	  multiply_by(100) %>%
+	  round(2) %>%
+	  format(nsmall=2) %>%
+	  paste0('%')
+}
